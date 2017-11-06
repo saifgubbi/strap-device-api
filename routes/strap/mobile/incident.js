01573ee8f4e14eb1cec4b18e0c3f34c13ff03515
saifgubbi <saifgubbi@gmail.com>
2017-11-06 15:51:37 +0530

    06Nov2017 3:51PM

--- routes/strap/mobile/incident.js
+++ routes/strap/mobile/incident.js
@@ -3,97 +3,55 @@ var router = express.Router();
 var op = require('../../../oracleDBOps');
 var async = require('async');
 var oracledb = require('oracledb');
+var multer = require('multer');
+var fs=require('fs');
+var path=require('path');
 
 router.post('/', function (req, res) {
-    pickIncident(req, res);
+    createIncident(req, res);
+});
+
+router.get('/', function (req, res) {
+    getImage(req, res);
 });
 
 module.exports = router;
 
-var fs = require('fs');
 
-// function to encode file data to base64 encoded string
-function base64_encode(file) {
-    var bitmap = fs.readFileSync(file);
-    return new Buffer(bitmap).toString('base64');
-}
-
-function pickIncident(req, res) {
-    let userId = req.body.userId;
-    let locId = req.body.locId;
-    let partGrp = req.body.partGrp;
-
-    let bindArr = [];
-
-    let sqlStatement = "INSERT INTO INCIDENTS_T VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20) ";
-
-    req.body.objArray.forEach(function (obj) {
-        let binVars;
-        binVars = [obj.eventId, obj.eventType, obj.eventName, new Date(), locId, obj.message, obj.priority,partGrp,obj.comments,base64_encode(obj.image),userId];
-        bindArr.push(binVars);
-    });
-    insertEvents(req, res, sqlStatement, bindArr);
-}
-
-function insertEvents(req, res, sqlStatement, bindArr) {
-
-    let errArray = [];
-    let doneArray = [];
-
-    var doConnect = function (cb) {
-        op.doConnectCB(function (err, conn) {
-            cb(null, conn);
-        });
-    };
-
-    function doInsert(conn, cb) {
-        console.log("In  doInsert");
-        let arrayCount = 1;
-        async.eachSeries(bindArr, function (data, callback) {
-            arrayCount++;
-            console.log("Inserting :", JSON.stringify(data));
-            let insertStatement = sqlStatement;
-            let bindVars = data;
-            conn.execute(insertStatement
-                    , bindVars, {
-                        autoCommit: true// Override the default non-autocommit behavior
-                    }, function (err, result)
-            {
-                if (err) {
-                    console.log("Error Occured: ", err);
-                    errArray.push({row: arrayCount, err: err});
-                    callback();
-                } else {
-                    console.log("Rows inserted: " + result.rowsAffected); // 1                    
-                    doneArray.push({row: arrayCount});
-                    callback();
-                }
-            });
-        }, function (err) {
-            if (err) {
-                console.log("Event Insert Error");
-                res.writeHead(500, {'Content-Type': 'application/json'});
-                errArray.push({row: 0, err: err});
-                res.end(`err:${err}}`);
-            } else {
-                res.json({"total": bindArr.length, "success": doneArray.length, "err": errArray.length, "errMsg": errArray});
-            }
-            cb(null, conn);
-        }
-        );
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        console.log(file.fieldname);
+        callback(null, file.fieldname + "_" + Date.now());
     }
 
-    async.waterfall(
-            [doConnect,
-                doInsert
-            ],
-            function (err, conn) {
-                if (err) {
-                    console.error("In waterfall error cb: ==>", err, "<==");
-                    res.status(500).json({message: err});
-                }
-                console.log("Done Waterfall");
-                if (conn)
-                    conn.close();
-            });
-}
+});
+
+
+var upload = multer({
+    storage: Storage
+}).single("file"); //Field name and max count
+
+ function createIncident (req, res) {
+    upload(req, res, function (err) {
+        console.log(req.body);
+        if (err) {
+            console.log(err);
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+
+};
+
+function getImage (req, res) {
+    var storedMimeType = 'image/jpeg';
+    res.setHeader('Content-Type', storedMimeType);
+    fs.createReadStream(path.join('./images/', req.params.id).replace(/\.[^/.]+$/, "")).pipe(res)
+};
+
+
+
+
